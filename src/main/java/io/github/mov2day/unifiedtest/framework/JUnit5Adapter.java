7f60df56498b39de17a3741d649662f0382a6964
mov2day <mov2day@gmail.com>
2025-04-24 20:11:32 +0200

    Bump version to 0.1.1 and enhance JUnit5Adapter with listener registration

--- src/main/java/io/github/mov2day/unifiedtest/framework/JUnit5Adapter.java
+++ src/main/java/io/github/mov2day/unifiedtest/framework/JUnit5Adapter.java
@@ -4,18 +4,24 @@ import org.gradle.api.Project;
 import org.gradle.api.tasks.testing.Test;
 import io.github.mov2day.unifiedtest.collector.UnifiedTestResultCollector;
 import io.github.mov2day.unifiedtest.reporting.ConsoleReporter;
+import io.github.mov2day.unifiedtest.reporting.UnifiedJUnit5Listener;
+import io.github.mov2day.unifiedtest.reporting.PrettyConsoleTestListener;
 
 public class JUnit5Adapter implements TestFrameworkAdapter {
     @Override
     public boolean isApplicable(Project project) {
         return FrameworkDetector.detect(project).contains("JUnit5");
     }
+
     @Override
     public void registerListeners(Project project, Test testTask, UnifiedTestResultCollector collector, ConsoleReporter reporter) {
         testTask.useJUnitPlatform();
-        // JUnit5 listeners are typically registered via ServiceLoader or system property.
-        // For demonstration, you may need to instruct users to register UnifiedJUnit5Listener via ServiceLoader or system property.
+        // Initialize the static collector and reporter in UnifiedJUnit5Listener
+        UnifiedJUnit5Listener.setCollectorAndReporter(collector, reporter);
+        // Add the test listener using Gradle's test listener API
+        testTask.addTestListener(new PrettyConsoleTestListener(project, "standard"));
     }
+
     @Override
     public String getName() { return "JUnit5"; }
 }
